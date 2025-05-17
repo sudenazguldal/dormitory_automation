1773ba9c578f59caa94e3d363ff044a357def19a
Sudenazguldal <sude.guldal29@gmail.com>
2025-05-18 02:53:43 +0300

    LAST

--- config/db_config.php
+++ config/db_config.php
@@ -1,7 +1,7 @@
 <?php
 
 $host     = '127.0.0.1';
-$port     = 3307;               // XAMPP’in MySQL portu
+$port     = 3306;               // XAMPP’in MySQL portu
 $dbname   = 'dormitory_database'; // Veritabanı adı
 $username = 'root';
 $password = '';                 // phpMyAdmin şifresizse boş bırakın
