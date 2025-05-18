76195c0f6a8aca5260edd8dbab6e18522696b190
Merve Gazioglu <gazioglumerve4@gmail.com>
2025-05-18 14:52:05 +0300

    congif dosyasi port degisikligi

---
+++ config/db_config.php
@@ -0,0 +1,9 @@
+<?php
+
+$host     = '127.0.0.1';
+$port     = 3307;               // XAMPP’in MySQL portu
+$dbname   = 'dormitory_database'; // Veritabanı adı
+$username = 'root';
+$password = '';                 // phpMyAdmin şifresizse boş bırakın
+
+?>
